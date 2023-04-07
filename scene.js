b4a19ac47876d4c21f3c49daf7cd70a5224e5749
Yubin <yoobintse@gmail.com>
2023-04-07 16:04:34 -0400

    Add files via upload

---
+++ scene.js
@@ -0,0 +1,37 @@
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add a cube to the scene
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Position the camera
+camera.position.z = 5;
+
+// Add a light source
+const light = new THREE.PointLight(0xffffff, 1, 1000);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+// Add controls for interactivity
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Animation loop
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    // Rotate the cube
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Render the scene
+    renderer.render(scene, camera);
+};
+
+animate();
